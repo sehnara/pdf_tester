199391130acd437b7475b6a030cb3871691a71d4
sehoon <gsh723@douzone.com>
2021-12-08 10:48:30 +0900

    [Pagenation] PDF 파일 페이지네이션 구현

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Pressable, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import PdfView from "./src/pdf";
 
 export default function App() {
+  const onDownLoad = () => {};
+  const onShare = () => {};
+  const URL = "http://samples.leanpub.com/thereactnativebook-sample.pdf";
+  const [page, setPage] = useState([0, 0]);
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ zIndex: 100 }}>
+        <Text>{`${page[0]}/${page[1]} 페이지`}</Text>
+      </View>
       <View style={styles.container}>
-        <PdfView />
+        <PdfView url={URL} setPage={setPage} />
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          padding: 20,
+
+          justifyContent: "space-between",
+        }}
+      >
+        <Pressable onPress={onDownLoad}>
+          <Text>다운로드</Text>
+        </Pressable>
+        <Pressable onPress={onShare}>
+          <Text style={{ color: "blue" }}>공유하기</Text>
+        </Pressable>
       </View>
     </SafeAreaView>
   );
@@ -19,6 +41,6 @@ const styles = StyleSheet.create({
     backgroundColor: "rgb(165,165,165)",
     alignItems: "center",
     justifyContent: "center",
-    padding: 20,
+    paddingTop: 20,
   },
 });
--- src/pdf.jsx
+++ src/pdf.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React,{ useState,useEffect } from 'react'
 import { View,Text,StyleSheet,Dimensions } from 'react-native';
 import Pdf from 'react-native-pdf';
 
-const PdfView = () =>{
+const PdfView = (props) =>{
+    const {url, setPage} = props;
+    const source = {uri : url, cache: true};
 
-    const source = {uri : 'http://samples.leanpub.com/thereactnativebook-sample.pdf', cache: true};
+    const [pages, setPages] = useState(0);
+    const [totalPages, setTotalPages]=useState(0);
 
-    return <Pdf 
+    useEffect(()=>{
+        setPage([pages, totalPages]);
+    },[pages,totalPages])
+
+    return <View>
+        <Pdf 
             source={source} 
-            onLoadComplete={(numberOfPages, filePath)=>{console.log(`Number of Pages : ${numberOfPages}`)}} 
-            onPageChanged={(page, numberOfPages)=>{console.log(`Current page : ${page}`)}} 
+            onLoadComplete={(numberOfPages, filePath)=>{
+                console.log(`Number of Pages : ${numberOfPages}`);
+                setTotalPages(numberOfPages);
+            }} 
+            onPageChanged={(page, numberOfPages)=>{
+                console.log(`Current page : ${page}`)
+                setPages(page)
+            }} 
             onError={(error)=>{console.log(error)}}
             onPressLink = {(uri) =>{console.log(`Link pressed : ${uri}`)}}
             style={styles.pdf}
         />
+    </View>
 }
 const styles = StyleSheet.create({
     pdf: {
